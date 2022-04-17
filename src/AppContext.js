6e58e65966263073b968d984111e6ba04dea5c90
jermuk <miqoo1996@mail.ru>
2022-04-18 00:40:33 +0400

    server version

--- src/AppContext.js
+++ src/AppContext.js
@@ -1,7 +1,7 @@
 import {createContext} from "react";
 
 export const AppContextDefaultValue = {
-    apiUrl: "http://127.0.0.1:8888/api/calendar",
+    apiUrl: "http://159.65.151.204:8888/api/calendar",
 };
 
 export const AppContext = createContext(AppContextDefaultValue);
