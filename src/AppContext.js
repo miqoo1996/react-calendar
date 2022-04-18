87cf37759258dddef1c98ced5410eb2cbae0282f
jermuk <miqoo1996@mail.ru>
2022-04-18 20:02:49 +0400

    build

--- src/AppContext.js
+++ src/AppContext.js
@@ -1,7 +1,7 @@
 import {createContext} from "react";
 
 export const AppContextDefaultValue = {
-    apiUrl: "http://159.65.151.204:8888/api/calendar",
+    apiUrl: "http://cal.loc/api/calendar",
 };
 
 export const AppContext = createContext(AppContextDefaultValue);
