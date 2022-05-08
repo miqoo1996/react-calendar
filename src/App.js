f65348641b5ef7e868e04aa46076719482963ae8
jermuk <miqoo1996@mail.ru>
2022-05-09 03:11:56 +0400

    improvements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './public/App.scss';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Agencies from './components/agency/Agencies';
 import Booking from "./components/booking/Booking";
 import PageNoMatch from "./PageNoMatch";
 import {AppContext, AppContextDefaultValue} from "./AppContext";
@@ -24,8 +23,6 @@ function App() {
               <div className="App">
                   <div className="container-fluid">
                       <Routes>
-                          {/*<Route exact path="/" element={<Agencies />} />*/}
-                          <Route exact path="/" element={<Questionnaire />} />
                           <Route exact path="/:slug" element={<Questionnaire />} />
                           <Route exact path="/book-call/:eventId/:ids" element={<Booking />} />
                           <Route path="*" element={<PageNoMatch><h1 className="text-center page-title top-50">404 Page Not Found.</h1></PageNoMatch>} />
