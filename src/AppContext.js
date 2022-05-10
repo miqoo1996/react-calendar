072db6c1ec1cdcf1d1fbee1b1e2ae71199acf73d
Michael <mikaeli@localexpress.io>
2022-05-10 18:39:00 +0400

    changed backend enpoint

--- src/AppContext.js
+++ src/AppContext.js
@@ -1,7 +1,7 @@
 import {createContext} from "react";
 
 export const AppContextDefaultValue = {
-    apiUrl: "http://159.65.151.204:8888/api",
+    apiUrl: "https://booking-call-app.herokuapp.com/api",
     GOOGLE_SCOPES: "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar.readonly https://www.googleapis.com/auth/calendar.events https://www.googleapis.com/auth/calendar",
     GOOGLE_API_KEY: "AIzaSyDEujdZt-SPte2quj583OQbidQwvMMFsRo",
     GOOGLE_CLIENT_ID: "113089812934-o1j0pv612t3sonmgjm6ffkf9538a38ss.apps.googleusercontent.com",
